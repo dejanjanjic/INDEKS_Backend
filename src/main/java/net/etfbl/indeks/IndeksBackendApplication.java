2424feab90762150e73493773a6eca76d778cfc4
dejanjanjic <dejan.janjic@student.etf.unibl.org>
2024-09-05 18:10:09 +0200

    init commit

---
+++ src/main/java/net/etfbl/indeks/IndeksBackendApplication.java
@@ -0,0 +1,13 @@
+package net.etfbl.indeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndeksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndeksBackendApplication.class, args);
+	}
+
+}
