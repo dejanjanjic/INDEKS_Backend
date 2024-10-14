38bb9f4d8e664582aab9a352f02cd2a9bf01a6dc
dejanjanjic <dejan.janjic@student.etf.unibl.org>
2024-10-14 11:21:30 +0200

    test

--- src/main/java/net/etfbl/indeks/IndeksBackendApplication.java
+++ src/main/java/net/etfbl/indeks/IndeksBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IndeksBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(IndeksBackendApplication.class, args);
 	}
