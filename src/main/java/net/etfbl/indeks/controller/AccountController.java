4b1c21e7726f42723ae3aa630074864ad5b399db
dejanjanjic <dejan.janjic@student.etf.unibl.org>
2024-10-17 19:04:42 +0200

    Account tested and finished

--- src/main/java/net/etfbl/indeks/controller/AccountController.java
+++ src/main/java/net/etfbl/indeks/controller/AccountController.java
@@ -3,9 +3,11 @@ package net.etfbl.indeks.controller;
 import net.etfbl.indeks.model.Account;
 import net.etfbl.indeks.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/account")
@@ -18,17 +20,48 @@ public class AccountController {
     }
 
     @GetMapping
-    public List<Account> getAccounts(){
-        return accountService.getAccounts();
+    public ResponseEntity<List<Account>> getAccounts(){
+        return ResponseEntity.ok(accountService.getAccounts());
+    }
+
+    @GetMapping(path = "{accountId}")
+    public ResponseEntity<Account> getAccountById(@PathVariable(name = "accountId")Long accountId){
+        Optional<Account> account = accountService.getAccountById(accountId);
+        if(account.isPresent()){
+            return ResponseEntity.ok(account.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public void registerNewAccount(@RequestBody Account account){
-        accountService.addNewAccount(account);
+    public ResponseEntity<Account> registerNewAccount(@RequestBody Account account){
+        Account temp = accountService.addNewAccount(account);
+        if(temp != null){
+            return ResponseEntity.ok(temp);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping(path = "{accountId}")
-    public void deleteAccount(@PathVariable("accountId")Long accountId){
-        accountService.deleteAccount(accountId);
+    public ResponseEntity deleteAccount(@PathVariable("accountId")Long accountId){
+        boolean deleted = accountService.deleteAccount(accountId);
+        if(deleted){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
+
+    @PutMapping
+    public ResponseEntity updateAccount(@RequestBody Account account){
+        boolean updated = accountService.updateAccount(account);
+        if(updated){
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
