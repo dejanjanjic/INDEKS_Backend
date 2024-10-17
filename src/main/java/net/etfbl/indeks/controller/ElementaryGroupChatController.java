71f6572761358707a0d88135e1d0c8c9c9f00e72
MiLaNIliSkOvIc <milan.iliskovic@student.etf.unibl.org>
2024-10-17 18:42:49 +0200

    ElementaryGroupChat REsponse added

--- src/main/java/net/etfbl/indeks/controller/ElementaryGroupChatController.java
+++ src/main/java/net/etfbl/indeks/controller/ElementaryGroupChatController.java
@@ -1,17 +1,19 @@
 package net.etfbl.indeks.controller;
 
-
 import net.etfbl.indeks.model.ElementaryGroupChat;
 import net.etfbl.indeks.service.ElementaryGroupChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path="api/v1/elementaryGroup")
+@RequestMapping(path = "api/v1/elementaryGroup")
 public class ElementaryGroupChatController {
+
     private final ElementaryGroupChatService elementaryGroupChatService;
 
     @Autowired
@@ -19,32 +21,47 @@ public class ElementaryGroupChatController {
         this.elementaryGroupChatService = elementaryGroupChatService;
     }
 
-
     @GetMapping
-    public List<ElementaryGroupChat> getPrivateGroupChats() {
-        return elementaryGroupChatService.getElementaryGroupChats();
+    public ResponseEntity<List<ElementaryGroupChat>> getElementaryGroupChats() {
+        List<ElementaryGroupChat> chats = elementaryGroupChatService.getElementaryGroupChats();
+        return new ResponseEntity<>(chats, HttpStatus.OK);
     }
+
     @GetMapping(path = "{groupId}")
-    public Optional<ElementaryGroupChat> getPrivateGroupChat(@PathVariable("groupId")Long groupId) {
-        return elementaryGroupChatService.getGroup(groupId);
+    public ResponseEntity<Optional<ElementaryGroupChat>> getElementaryGroupChat(@PathVariable("groupId") Long groupId) {
+        Optional<ElementaryGroupChat> chat = elementaryGroupChatService.getGroup(groupId);
+        if (chat.isPresent()) {
+            return new ResponseEntity<>(chat, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
     @PostMapping
-    public void registerNewPrivateGroupChat(@RequestBody ElementaryGroupChat group){
+    public ResponseEntity<String> registerNewElementaryGroupChat(@RequestBody ElementaryGroupChat group) {
         elementaryGroupChatService.addNewElementaryGroupChat(group);
+        return new ResponseEntity<>("New ElementaryGroupChat created successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "{groupId}")
-    public void deleteGroup(@PathVariable("groupId")Long groupId){
-        elementaryGroupChatService.deleteGroup(groupId);
+    public ResponseEntity<String> deleteGroup(@PathVariable("groupId") Long groupId) {
+        boolean isDeleted = elementaryGroupChatService.deleteGroup(groupId);
+        if (isDeleted) {
+            return new ResponseEntity<>("Group deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Group not found", HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PutMapping(path="{groupId}")
-    public void updateGroup(
+    @PutMapping(path = "{groupId}")
+    public ResponseEntity<String> updateGroup(
             @PathVariable("groupId") Long groupId,
-            @RequestParam(required = false) String groupName){
-        elementaryGroupChatService.updateElementaryChatGroup(groupId, groupName);
+            @RequestParam(required = false) String groupName) {
+        boolean isUpdated = elementaryGroupChatService.updateElementaryChatGroup(groupId, groupName);
+        if (isUpdated) {
+            return new ResponseEntity<>("Group updated successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Group not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
-
-
-
