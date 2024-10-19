b9c6d229212d76ec12fd6f28aa691f4b7ed7d612
Mihajlo <mihajloseva13@gmail.com>
2024-10-19 18:04:27 +0200

    TutorAccount added - ?

--- src/main/java/net/etfbl/indeks/controller/TutorAccountController.java
+++ src/main/java/net/etfbl/indeks/controller/TutorAccountController.java
@@ -1,4 +1,58 @@
 package net.etfbl.indeks.controller;
 
+import net.etfbl.indeks.dto.AddUserAccountDTO;
+import net.etfbl.indeks.model.TutorAccount;
+import net.etfbl.indeks.service.TutorAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/tutorAccount")
 public class TutorAccountController {
+
+    private final TutorAccountService tutorAccountService;
+
+    @Autowired
+    public TutorAccountController(TutorAccountService tutorAccountService) {
+        this.tutorAccountService = tutorAccountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TutorAccount>> getAccounts() {
+        return ResponseEntity.ok(tutorAccountService.getTutorAccounts());
+    }
+
+    @GetMapping(path = "{accountId}")
+    public ResponseEntity<TutorAccount> getTutorAccountById(@PathVariable(name = "accountId") Long accountId) {
+        Optional<TutorAccount> tutorAccount = tutorAccountService.getTutorAccountById(accountId);
+        if (tutorAccount.isPresent()) {
+            return ResponseEntity.ok(tutorAccount.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<TutorAccount> registerNewTutorAccount(@RequestBody AddUserAccountDTO addUserAccountDTO) {
+        TutorAccount newAccount = tutorAccountService.addNewTutorAccount(addUserAccountDTO);
+        if (newAccount != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(newAccount);
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+    @DeleteMapping(path = "{accountId}")
+    public ResponseEntity<Void> deleteTutorAccount(@PathVariable("accountId") Long accountId) {
+        boolean deleted = tutorAccountService.deleteTutorAccount(accountId);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/net/etfbl/indeks/model/TutorAccount.java
+++ src/main/java/net/etfbl/indeks/model/TutorAccount.java
@@ -1,5 +1,39 @@
 package net.etfbl.indeks.model;
 
-public class TutorAccount {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+@Table
+public class TutorAccount {
+    @Id
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name="id")
+    private UserAccount userAccount;
+
+    @OneToMany
+    private List<TutoringOffer> tutoringOffers;
+
+    public TutorAccount(){
+
+    }
+
+    public TutorAccount(UserAccount userAccount){
+        this.userAccount = userAccount;
+        //this.id = userAccount.getAccountId();
+    }
+
+
+    public UserAccount getUserAccount(){ return userAccount;}
+
+    public void setUserAccount(UserAccount userAccount){
+        this.userAccount = userAccount;
+        //this.id = userAccount.getAccountId();
+    }
+
+    public Long getId(){ return id;}
 }
