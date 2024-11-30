fb6733ac78adfb9ac5d854bc397f7f9e613a7917
DESKTOP-1C7EODV\WIN10 <igorpiljagic1@gmail.com>
2024-11-30 13:05:57 +0100

    zakomentarisane pom.xml zavisnosti

--- src/main/java/net/etfbl/indeks/model/Account.java
+++ src/main/java/net/etfbl/indeks/model/Account.java
@@ -1,8 +1,8 @@
 package net.etfbl.indeks.model;
 
 import jakarta.persistence.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
