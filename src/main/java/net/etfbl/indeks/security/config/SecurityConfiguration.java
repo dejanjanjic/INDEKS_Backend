ec614b1e97b51027ef306082fb680b16f9c0a500
DESKTOP-1C7EODV\WIN10 <igorpiljagic1@gmail.com>
2024-12-07 18:07:21 +0100

    Dorada login i registration logike.

--- src/main/java/net/etfbl/indeks/security/config/SecurityConfiguration.java
+++ src/main/java/net/etfbl/indeks/security/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
         http.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/**") //TODO ONEMOGUCITI PUTANJE
                 .permitAll()
                 .anyRequest()
                 .authenticated()
